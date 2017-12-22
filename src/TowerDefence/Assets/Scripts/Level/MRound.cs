a26ef71df20c43acc4c9f829bd0247cd66a8b39c
Desire <634488804@qq.com>
2017-12-22 18:38:16 +0800

    delete

--- src/TowerDefence/Assets/Scripts/Level/MRound.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-class MRound
-{
-    public List<KeyValuePair<CommonMonster,int>> MonsterList = new List<KeyValuePair<CommonMonster, int>>();
-}
