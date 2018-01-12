05b9c7c2adc563943b5de2e8a6921215906e0527
Desire <634488804@qq.com>
2018-01-12 20:43:04 +0800

    界面修复，关卡文件读取修复

---
+++ src/Luobo/Assets/Game/Scripts/Application/2.View/3.Controller/CountDownCompleteCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class CountDownCompleteCommand : Controller
+{
+    public override void Execute(object data)
+    {
+        //开始游戏
+        GameModel gModel = GetModel<GameModel>();
+        gModel.IsPlaying = true;
+
+        //开始出怪
+        RoundModel rModel = GetModel<RoundModel>();
+        if(rModel == null)
+            Debug.LogAssertion("RoundModel为空！!");
+        Debug.Log("CountDownComplete!");
+        rModel.StartRound();
+    }
+}
---
+++ src/Luobo/Assets/Game/Scripts/Application/2.View/3.Controller/StartRoundCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+class StartRoundCommand : Controller
+{
+    public override void Execute(object data)
+    {
+        StartRoundArgs e = data as StartRoundArgs;
+        GameObject.Find("Canvas").transform.Find("UIBoard").GetComponent<UIBoard>().OnRoundStart(e);
+    }
+}
